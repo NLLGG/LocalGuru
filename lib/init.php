9d9d2b54bd42c33c18ce8cb8d226bce75180a228
Rutger van Sleen <git@djslash.org>
2015-10-13 09:38:42 +0200

    Sanitize GET keys

--- lib/init.php
+++ lib/init.php
@@ -6,18 +6,19 @@ require MAINDIR.'/settings.php';
 require LIBDIR.'/functions.php';
 require PARTY.'/autoload.php';
 
-$search = array(
-	"address" => "",
-	"country" => "",
-	"radius"  => "",
-	"distros" => "",
-	"desktops" => "",
-	"actions" => "",
-	"groups" => "",
-	"targets" => "",
-	"rewards" => ""
+$getkeys = array(
+        'address' => FILTER_SANITIZE_ENCODED,
+        'country' => FILTER_SANITIZE_ENCODED,
+        'radius'  => FILTER_VALIDATE_INT,
+        'distros' => FILTER_SANITIZE_ENCODED,
+        'desktops' => FILTER_SANITIZE_ENCODED,
+        'actions' => FILTER_SANITIZE_ENCODED,
+        'groups' => FILTER_SANITIZE_ENCODED,
+        'targets' => FILTER_SANITIZE_ENCODED,
+        'rewards' => FILTER_SANITIZE_ENCODED 
 );
 
-process_GET($search);
+//process_GET($search);
+$search = filter_input_array(INPUT_GET, $getkeys, true);
 
 ?>
